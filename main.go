47e551995a09d2efbef0d8e9b543d16b59691366
Morten Vistisen <github@mbvlabs.com>
2025-09-09 19:04:40 +0200

    chore: include version in install (#7)

--- main.go
+++ main.go
@@ -3,23 +3,28 @@ package main
 import (
 	"context"
 	"log"
-	"github.com/mbvlabs/andurel/cli"
 	"os"
+	"runtime/debug"
 	"time"
+
+	"github.com/mbvlabs/andurel/cli"
 )
 
-var (
-	version string
-	date    = time.Now().Format("2006-01-02")
-)
+var date = time.Now().Format("2006-01-02")
+
+func getVersion() string {
+	if buildInfo, ok := debug.ReadBuildInfo(); ok {
+		if buildInfo.Main.Version != "(devel)" && buildInfo.Main.Version != "" {
+			return buildInfo.Main.Version
+		}
+	}
+	return "dev"
+}
 
 func main() {
 	ctx := context.Background()
 
-	if version == "" {
-		version = "dev"
-	}
-
+	version := getVersion()
 	rootCmd := cli.NewRootCommand(version, date)
 
 	if err := rootCmd.ExecuteContext(ctx); err != nil {
