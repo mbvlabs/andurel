d01fcea395358f998e3090a62159fe47f8cdc683
mbv <github@mbvlabs.com>
2025-09-11 14:25:52 +0200

    updates

--- layout/layout.go
+++ layout/layout.go
@@ -4,14 +4,14 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
-	"io"
-	"io/fs"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
 	"strings"
+	"text/template"
 
+	"github.com/mbvlabs/andurel/layout/templates"
 	"github.com/mbvlabs/andurel/pkg/constants"
 )
 
@@ -20,6 +20,15 @@ const moduleName = "github.com/mbvlabs/andurel/layout/elements"
 type Element struct {
 	RootDir string
 	SubDirs []Element
+}
+
+type TemplateData struct {
+	ModuleName              string
+	Database                string
+	SessionKey              string
+	SessionEncryptionKey    string
+	TokenSigningKey         string
+	PasswordSalt           string
 }
 
 var Layout = []Element{
@@ -93,23 +102,35 @@ var Layout = []Element{
 }
 
 func Scaffold(targetDir, projectName string) error {
-	elementsDir := filepath.Join(filepath.Dir(getCurrentFile()), "elements")
+	return ScaffoldWithDatabase(targetDir, projectName, "postgresql")
+}
+
+func ScaffoldWithDatabase(targetDir, projectName, database string) error {
+	templateData := TemplateData{
+		ModuleName:              projectName,
+		Database:                database,
+		SessionKey:              generateRandomHex(64),
+		SessionEncryptionKey:    generateRandomHex(32),
+		TokenSigningKey:         generateRandomHex(32),
+		PasswordSalt:           generateRandomHex(16),
+	}
 
 	if err := os.MkdirAll(targetDir, constants.DirPermissionDefault); err != nil {
 		return fmt.Errorf("failed to create target directory: %w", err)
 	}
 
-	if err := copyRootLevelFiles(elementsDir, targetDir, projectName); err != nil {
-		return fmt.Errorf("failed to copy root-level files: %w", err)
+	if err := processTemplatedFiles(targetDir, templateData); err != nil {
+		return fmt.Errorf("failed to process templated files: %w", err)
 	}
 
 	if err := createGoMod(targetDir, projectName); err != nil {
 		return fmt.Errorf("failed to create go.mod: %w", err)
 	}
 
+	// Create empty directories structure
 	for _, element := range Layout {
-		if err := createElementStructure(targetDir, elementsDir, element, projectName); err != nil {
-			return fmt.Errorf("failed to create element %s: %w", element.RootDir, err)
+		if err := createDirectoryStructure(targetDir, element); err != nil {
+			return fmt.Errorf("failed to create directory structure %s: %w", element.RootDir, err)
 		}
 	}
 
@@ -124,81 +145,148 @@ func Scaffold(targetDir, projectName string) error {
 	return nil
 }
 
-func createElementStructure(
-	targetDir, elementsDir string,
-	element Element,
-	projectName string,
-) error {
-	elementTargetPath := filepath.Join(targetDir, element.RootDir)
-	elementSourcePath := filepath.Join(elementsDir, element.RootDir)
-
-	if err := os.MkdirAll(elementTargetPath, constants.DirPermissionDefault); err != nil {
-		return fmt.Errorf("failed to create directory %s: %w", elementTargetPath, err)
+func processTemplatedFiles(targetDir string, data TemplateData) error {
+	// Template mappings: template file -> target file path
+	templateMappings := map[string]string{
+		// Core files
+		"database.tmpl":        "database/psql.go",
+		"env.tmpl":            ".env.example", 
+		"database_config.tmpl": "config/database.go",
+		"sqlc.tmpl":           "database/sqlc.yaml",
+		"gitignore.tmpl":      ".gitignore",
+		"justfile.tmpl":       "justfile",
+		
+		// Assets
+		"assets_assets.tmpl":     "assets/assets.go",
+		"assets_css_tw.tmpl":     "assets/css/tw.css",
+		"assets_js_scripts.tmpl": "assets/js/scripts.js",
+		"assets_js_datastar.tmpl": "assets/js/datastar_1-0-0-rc5.min.js",
+		
+		// CSS
+		"css_base.tmpl":  "css/base.css",
+		"css_theme.tmpl": "css/theme.css",
+		
+		// Commands
+		"cmd_app_main.tmpl":     "cmd/app/main.go",
+		"cmd_migrate_main.tmpl": "cmd/migrate/main.go",
+		
+		// Config
+		"config_app.tmpl":    "config/app.go",
+		"config_auth.tmpl":   "config/auth.go", 
+		"config_config.tmpl": "config/config.go",
+		
+		// Controllers
+		"controllers_api.tmpl":        "controllers/api.go",
+		"controllers_assets.tmpl":     "controllers/assets.go",
+		"controllers_controller.tmpl": "controllers/controller.go",
+		"controllers_pages.tmpl":      "controllers/pages.go",
+		
+		// Database
+		"database_migrations_gitkeep.tmpl": "database/migrations/.gitkeep",
+		"database_queries_gitkeep.tmpl": "database/queries/.gitkeep",
+		
+		// Models
+		"models_errors.tmpl":    "models/errors.go",
+		"models_validator.tmpl": "models/validator.go",
+		
+		// Router
+		"router_router.tmpl":           "router/router.go",
+		"router_cookies_cookies.tmpl":  "router/cookies/cookies.go",
+		"router_cookies_flash.tmpl":    "router/cookies/flash.go",
+		"router_middleware_logging.tmpl": "router/middleware/logging.go",
+		
+		// Routes
+		"router_routes_routes.tmpl":  "router/routes/routes.go",
+		"router_routes_api.tmpl":     "router/routes/api.go",
+		"router_routes_assets.tmpl":  "router/routes/assets.go",
+		"router_routes_pages.tmpl":   "router/routes/pages.go",
+		
+		// Views
+		"views_layout.tmpl":       "views/layout.templ",
+		"views_home.tmpl":         "views/home.templ",
+		"views_bad_request.tmpl":  "views/bad_request.templ",
+		"views_internal_error.tmpl": "views/internal_error.templ",
+		"views_not_found.tmpl":    "views/not_found.templ",
+		
+		// View Components
+		"views_components_head.tmpl":   "views/components/head.templ",
+		"views_components_toasts.tmpl": "views/components/toasts.templ",
+		
+		// Generated Template Files
+		"views_layout_templ.tmpl":       "views/layout_templ.go",
+		"views_home_templ.tmpl":         "views/home_templ.go",
+		"views_bad_request_templ.tmpl":  "views/bad_request_templ.go",
+		"views_internal_error_templ.tmpl": "views/internal_error_templ.go",
+		"views_not_found_templ.tmpl":    "views/not_found_templ.go",
+		
+		// Generated Component Files
+		"views_components_head_templ.tmpl":   "views/components/head_templ.go",
+		"views_components_toasts_templ.tmpl": "views/components/toasts_templ.go",
 	}
 
-	if _, err := os.Stat(elementSourcePath); err == nil {
-		if err := copyDirectoryContents(elementSourcePath, elementTargetPath, projectName); err != nil {
-			return fmt.Errorf("failed to copy contents from %s: %w", elementSourcePath, err)
-		}
-	}
-
-	for _, subElement := range element.SubDirs {
-		if err := createElementStructure(elementTargetPath, elementSourcePath, subElement, projectName); err != nil {
-			return fmt.Errorf("failed to create sub-element %s: %w", subElement.RootDir, err)
+	for templateFile, targetPath := range templateMappings {
+		if err := processTemplate(targetDir, templateFile, targetPath, data); err != nil {
+			return fmt.Errorf("failed to process template %s: %w", templateFile, err)
 		}
 	}
 
 	return nil
 }
 
-func copyDirectoryContents(srcDir, destDir, projectName string) error {
-	return filepath.WalkDir(srcDir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		relPath, err := filepath.Rel(srcDir, path)
-		if err != nil {
-			return err
-		}
-
-		destPath := filepath.Join(destDir, relPath)
-
-		if d.IsDir() {
-			return os.MkdirAll(destPath, constants.DirPermissionDefault)
-		}
-
-		return copyFile(path, destPath, projectName)
-	})
-}
-
-func copyFile(src, dest, projectName string) error {
-	srcFile, err := os.Open(src)
+func processTemplate(targetDir, templateFile, targetPath string, data TemplateData) error {
+	// Read template content
+	content, err := templates.Files.ReadFile(templateFile)
 	if err != nil {
-		return err
-	}
-	defer srcFile.Close()
-
-	destFile, err := os.Create(dest)
-	if err != nil {
-		return err
-	}
-	defer destFile.Close()
-
-	content, err := io.ReadAll(srcFile)
-	if err != nil {
-		return err
+		return fmt.Errorf("failed to read template %s: %w", templateFile, err)
 	}
 
 	contentStr := string(content)
-	contentStr = strings.ReplaceAll(contentStr, moduleName, projectName)
-
-	if strings.HasSuffix(src, ".env.example") {
-		contentStr = replaceSecureValues(contentStr)
+	
+	// For generated *_templ.go files, replace hardcoded import paths
+	if strings.HasSuffix(templateFile, "_templ.tmpl") {
+		contentStr = strings.ReplaceAll(contentStr, moduleName, data.ModuleName)
 	}
 
-	_, err = destFile.WriteString(contentStr)
-	return err
+	// Parse and execute template
+	tmpl, err := template.New(templateFile).Parse(contentStr)
+	if err != nil {
+		return fmt.Errorf("failed to parse template %s: %w", templateFile, err)
+	}
+
+	// Create target directory if needed
+	fullTargetPath := filepath.Join(targetDir, targetPath)
+	if err := os.MkdirAll(filepath.Dir(fullTargetPath), constants.DirPermissionDefault); err != nil {
+		return fmt.Errorf("failed to create directory for %s: %w", targetPath, err)
+	}
+
+	// Create and write to target file
+	file, err := os.Create(fullTargetPath)
+	if err != nil {
+		return fmt.Errorf("failed to create file %s: %w", targetPath, err)
+	}
+	defer file.Close()
+
+	if err := tmpl.Execute(file, data); err != nil {
+		return fmt.Errorf("failed to execute template %s: %w", templateFile, err)
+	}
+
+	return nil
+}
+
+func createDirectoryStructure(targetDir string, element Element) error {
+	elementTargetPath := filepath.Join(targetDir, element.RootDir)
+	
+	if err := os.MkdirAll(elementTargetPath, constants.DirPermissionDefault); err != nil {
+		return fmt.Errorf("failed to create directory %s: %w", elementTargetPath, err)
+	}
+
+	for _, subElement := range element.SubDirs {
+		if err := createDirectoryStructure(elementTargetPath, subElement); err != nil {
+			return fmt.Errorf("failed to create sub-directory structure %s: %w", subElement.RootDir, err)
+		}
+	}
+
+	return nil
 }
 
 const goVersion = "1.24.4"
@@ -220,25 +308,6 @@ func runGoModTidy(targetDir string) error {
 	return cmd.Run()
 }
 
-func copyRootLevelFiles(elementsDir, targetDir, projectName string) error {
-	entries, err := os.ReadDir(elementsDir)
-	if err != nil {
-		return fmt.Errorf("failed to read elements directory: %w", err)
-	}
-
-	for _, entry := range entries {
-		if !entry.IsDir() {
-			srcPath := filepath.Join(elementsDir, entry.Name())
-			destPath := filepath.Join(targetDir, entry.Name())
-
-			if err := copyFile(srcPath, destPath, projectName); err != nil {
-				return fmt.Errorf("failed to copy file %s: %w", entry.Name(), err)
-			}
-		}
-	}
-
-	return nil
-}
 
 func initializeGit(targetDir string) error {
 	cmd := exec.Command("git", "init")
@@ -251,31 +320,6 @@ func getCurrentFile() string {
 	return filename
 }
 
-func replaceSecureValues(content string) string {
-	sessionKey := generateRandomHex(64)
-	sessionEncryptionKey := generateRandomHex(32)
-	tokenSigningKey := generateRandomHex(32)
-	passwordSalt := generateRandomHex(16)
-
-	content = strings.ReplaceAll(content, "SESSION_KEY=session_key", "SESSION_KEY="+sessionKey)
-	content = strings.ReplaceAll(
-		content,
-		"SESSION_ENCRYPTION_KEY=session_encryption_key",
-		"SESSION_ENCRYPTION_KEY="+sessionEncryptionKey,
-	)
-	content = strings.ReplaceAll(
-		content,
-		"TOKEN_SIGNING_KEY=token_signing_key",
-		"TOKEN_SIGNING_KEY="+tokenSigningKey,
-	)
-	content = strings.ReplaceAll(
-		content,
-		"PASSWORD_SALT=password_salt",
-		"PASSWORD_SALT="+passwordSalt,
-	)
-
-	return content
-}
 
 func generateRandomHex(bytes int) string {
 	randomBytes := make([]byte, bytes)
