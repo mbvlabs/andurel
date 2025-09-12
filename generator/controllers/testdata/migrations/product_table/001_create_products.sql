f4d0bd8e0b2da98339fe6689d7aa3d518ac11a55
mbv <github@mbvlabs.com>
2025-09-12 14:54:33 +0200

    tests

--- generator/controllers/testdata/migrations/product_table/001_create_products.sql
+++ generator/controllers/testdata/migrations/product_table/001_create_products.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
+    id uuid PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     description TEXT,
