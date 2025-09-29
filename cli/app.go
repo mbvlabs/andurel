24361bb0c1e776f350dcf1e61c588e888b834707
mbv <github@mbvlabs.com>
2025-09-29 17:36:34 +0200

    moving run cmd to be just andurel run

--- cli/app.go
+++ cli/app.go
@@ -1,10 +1,6 @@
 package cli
 
 import (
-	"os"
-	"os/exec"
-	"path/filepath"
-
 	"github.com/mbvlabs/andurel/layout"
 	"github.com/spf13/cobra"
 )
@@ -17,35 +13,9 @@ func newAppCommand() *cobra.Command {
 		Long:    "Commands related to running and managing the application.",
 	}
 
-	appCmd.AddCommand(newRunAppCommand())
 	appCmd.AddCommand(newTailwindCommand())
 
 	return appCmd
-}
-
-func newRunAppCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "run",
-		Short: "Runs the app",
-		Args:  cobra.ExactArgs(0),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			wd, err := os.Getwd()
-			if err != nil {
-				return err
-			}
-
-			binPath := filepath.Join(wd, "bin", "run")
-
-			runCmd := exec.Command(binPath)
-			runCmd.Stdout = os.Stdout
-			runCmd.Stderr = os.Stderr
-			runCmd.Stdin = os.Stdin
-
-			return runCmd.Run()
-		},
-	}
-
-	return cmd
 }
 
 func newTailwindCommand() *cobra.Command {
