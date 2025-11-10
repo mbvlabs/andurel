904d5c1e57ddafd70f2c92dc7b210e92e488506e
Morten Vistisen <github@mbvlabs.com>
2025-11-10 12:14:33 +0100

    feat: add backgrounds jobs for sqlite (#105)

--- layout/layout_test.go
+++ layout/layout_test.go
@@ -19,6 +19,9 @@ func TestProjectScaffoldingSqlite__GoldenFile(t *testing.T) {
 	if err := os.Setenv("ANDUREL_SKIP_BUILD", "true"); err != nil {
 		t.Fatalf("Failed to set ANDUREL_SKIP_TAILWIND env var: %v", err)
 	}
+	if err := os.Setenv("ANDUREL_TEST_MODE", "true"); err != nil {
+		t.Fatalf("Failed to set ANDUREL_TEST_MODE env var: %v", err)
+	}
 
 	tests := []struct {
 		name           string
