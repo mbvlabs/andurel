c6393e73d25785a5931677b369943f517ce59d74
Morten Vistisen <github@mbvlabs.com>
2025-11-24 12:36:54 +0100

    chore: ensure all cmds start from go mod (#145)

--- cli/migration.go
+++ cli/migration.go
@@ -109,12 +109,12 @@ func newMigrationDownToCommand() *cobra.Command {
 }
 
 func runMigrationBinary(args ...string) error {
-	wd, err := os.Getwd()
+	rootDir, err := findGoModRoot()
 	if err != nil {
 		return err
 	}
 
-	binPath := filepath.Join(wd, "bin", "migration")
+	binPath := filepath.Join(rootDir, "bin", "migration")
 	if _, err := os.Stat(binPath); err != nil {
 		if os.IsNotExist(err) {
 			return fmt.Errorf(
@@ -129,6 +129,7 @@ func runMigrationBinary(args ...string) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
+	cmd.Dir = rootDir
 
 	return cmd.Run()
 }
