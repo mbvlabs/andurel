0c956b81369a464bf384341f649c8f7b7ddbdbac
Morten Vistisen <github@mbvlabs.com>
2026-01-24 12:12:32 +0100

    fix: pass build version correctly (#362)

--- main.go
+++ main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"context"
 	"os"
-	"runtime/debug"
 	"time"
 
 	"github.com/mbvlabs/andurel/cli"
 )
 
-var date = time.Now().Format("2006-01-02")
+var (
+	version string
+	date    = time.Now().Format("2006-01-02")
+)
 
 func getVersion() string {
-	if buildInfo, ok := debug.ReadBuildInfo(); ok {
-		if buildInfo.Main.Version != "(devel)" && buildInfo.Main.Version != "" {
-			return buildInfo.Main.Version
-		}
+	if version != "" {
+		return version
 	}
 	return "dev"
 }
