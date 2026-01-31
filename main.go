255a65d4e8255b39e21213f46f9dadfc14ee8cb2
Morten Vistisen (MBV) <github@mbvlabs.com>
2026-01-31 13:58:47 +0100

    fix: version on install

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"runtime/debug"
 	"time"
 
 	"github.com/mbvlabs/andurel/cli"
@@ -16,6 +17,12 @@ var (
 func getVersion() string {
 	if version != "" {
 		return version
+	}
+
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			return info.Main.Version
+		}
 	}
 
 	return "dev"
