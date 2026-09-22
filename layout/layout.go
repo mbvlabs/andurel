8e735dbc9a5e8cec706e6081d8cc3ea8f0299181
Morten Vistisen <github@mbvlabs.com>
2026-09-22 11:11:26 +0200

    chore: re=intro agents.md

--- layout/layout.go
+++ layout/layout.go
@@ -177,6 +177,7 @@ var baseTemplateMappings = map[TmplTarget]TmplTargetPath{
 	"env.tmpl":       ".env.example",
 	"gitignore.tmpl": ".gitignore",
 	"readme.tmpl":    "README.md",
+	"agents.tmpl":    "AGENTS.md",
 
 	// Assets
 	"assets_assets.tmpl":      "assets/assets.go",
